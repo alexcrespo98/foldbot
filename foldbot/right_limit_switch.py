d03b845d6856b8970c73107b9bdb5303d483404b
alexcrespo98 <138143842+alexcrespo98@users.noreply.github.com>
2025-08-10 00:39:51 -0400

    Update right_limit_switch.py

--- foldbot/right_limit_switch.py
+++ foldbot/right_limit_switch.py
@@ -1,28 +1,22 @@
-# right_limit_switch.py
-# Publishes state of right (max X) limit switch
-
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool
-from pyfirmata import Arduino, util
-
-PIN_RIGHT_LIMIT = 10
+from std_msgs.msg import String
 
 class RightLimitSwitch(Node):
     def __init__(self):
         super().__init__('right_limit_switch')
-        self.board = Arduino('/arduino_rx')
-        self.limit_pin = self.board.digital[PIN_RIGHT_LIMIT]
-        self.limit_pin.mode = 0  # INPUT
-        self.pub = self.create_publisher(Bool, 'right_limit_state', 10)
-        self.timer = self.create_timer(0.05, self.read_switch)
+        self.tx_pub = self.create_publisher(String, 'arduino_tx', 10)
+        self.rx_sub = self.create_subscription(String, 'arduino_rx', self.arduino_rx_callback, 10)
+        self.query_switch()
 
-    def read_switch(self):
-        state = bool(self.limit_pin.read())
-        msg = Bool()
-        msg.data = state
-        self.pub.publish(msg)
-        self.get_logger().debug(f'Right limit: {state}')
+    def query_switch(self):
+        msg = String()
+        msg.data = "RIGHT_LIMIT_SWITCH:QUERY"
+        self.tx_pub.publish(msg)
+        self.get_logger().info("Sent: RIGHT_LIMIT_SWITCH:QUERY")
+
+    def arduino_rx_callback(self, msg):
+        self.get_logger().info(f"Arduino says: {msg.data}")
 
 def main(args=None):
     rclpy.init(args=args)
